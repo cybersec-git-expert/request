43edb45e7115eb340df1e94b886c4cc51f7c34fa
cybersec-git-expert <cyber.sec.expert@outlook.com>
2025-09-09 19:51:55 +0530

    Deploy backend to new EC2 instance 44.211.63.136

---
+++ working_entitlements.js
@@ -0,0 +1,4 @@
+// Working entitlements service - uses existing services/entitlements.js logic
+const { getEntitlements } = require('./services/entitlements');
+
+module.exports = { getEntitlements };
